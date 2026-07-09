62a0858588f6ecbde5e1868b2e3572038fc1b02e
Melon <dashjax9@gmail.com>
2026-07-09 10:48:48 -0500

    Added screen functionality using U8G2 lib

--- control/src/main.cpp
+++ control/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <SPI.h>
+#include <U8g2lib.h>
 
 #include "pins.hpp"
 #include "types.hpp"
@@ -23,6 +24,8 @@ MotorController motors[MOTOR_COUNT] = {
 int32_t motor_velocities[MOTOR_COUNT] = {};
 
 SPISettings settings(2000000, MSBFIRST, SPI_MODE3);
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 void kill_self();
 
@@ -57,28 +60,41 @@ void setup() {
   pinMode(KICKER_CSN_PIN, OUTPUT);
   digitalWrite(KICKER_CSN_PIN, HIGH);
   // End Initialize Kicker //
+
+  // Initialize Screen //
+  u8g2.begin();
+  // End Initialize Screen //
 }
 
 // Main control loop
 void loop() {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "Team: %d | ID: %d", team, id);
+  u8g2.drawStr(0,10, buf);
+  
   Serial.printf("Team: %d | ID: %d\n", team, id);
 
-  KickerCommand new_command;
-  new_command.charge_allowed = true;
-  new_command.kick_strength = 5;
-  new_command.kick_trigger = Breakbeam;
+  // KickerCommand new_command;
+  // new_command.charge_allowed = true;
+  // new_command.kick_strength = 5;
+  // new_command.kick_trigger = Breakbeam;
 
-  SPI1.beginTransaction(settings);
-  digitalWrite(KICKER_CSN_PIN, LOW);
+  // SPI1.beginTransaction(settings);
+  // digitalWrite(KICKER_CSN_PIN, LOW);
 
-  uint8_t response = SPI1.transfer(new_command.pack());
+  // uint8_t response = SPI1.transfer(new_command.pack());
 
-  digitalWrite(KICKER_CSN_PIN, HIGH);
-  SPI1.endTransaction();
+  // digitalWrite(KICKER_CSN_PIN, HIGH);
+  // SPI1.endTransaction();
 
-  KickerState ks = KickerState(response);
-  Serial.println("KS: " + ks.to_string());
+  // KickerState ks = KickerState(response);
+  // Serial.println("KS: " + ks.to_string());
 
+  
+  u8g2.sendBuffer();	
   delay(100);
 }
 
