889a1e9312da36c945a8a47578f9e4bf5b218809
Melon <dashjax9@gmail.com>
2026-07-20 17:10:46 -0500

    Reformated kicker docs to match.

--- control/lib/kicker/kicker.hpp
+++ control/lib/kicker/kicker.hpp
@@ -2,27 +2,35 @@
 
 #include <Arduino.h>
 
-/// @brief Type of kick to be performed by the kicker
+// Type of kick to be performed by the kicker
 enum ShootMode {
     Kick = 0,
     Chip = 1 << 7,
 };
 
-/// @brief Convert a ShootMode to a String
-/// @param type ShootMode to translate
-/// @return String format of mode
+/**
+ * Convert a `ShootMode` to a String
+ * 
+ * @param mode ShootMode to translate
+ * 
+ * @return String format of mode
+ */
 String shootmode_to_str(ShootMode mode);
 
-/// @brief What trigger will activate the kicker: None, Breakbeam, or Immediate
+// What trigger will activate the kicker: None, Breakbeam, or Immediate
 enum TriggerMode {
     Disabled = 0b11 << 5,
     Breakbeam = 1 << 5,
     Immediate = 1 << 6,
 };
 
-/// @brief Convert a TrigerMode to a String
-/// @param trigger TriggerMode to translate
-/// @return Stromg format of trigger
+/**
+ * Convert a TrigerMode to a String
+ * 
+ * @param trigger TriggerMode to translate
+ * 
+ * @return Stromg format of trigger
+ */
 String triggermode_to_str(TriggerMode trigger);
 
 // TODO: Copy and reformat table from rust docs
@@ -36,11 +44,18 @@ struct KickerCommand {
     // Whether or not kicker is allowed to charge
     bool charge_allowed = false;
 
-    /// @brief Converts kicker command into SPI formated byte to send
-    /// @return Byte to be sent to kicker
+    /**
+     * Converts kicker command into SPI formated byte to send
+     * 
+     * @return Byte to be sent to kicker
+     */
     uint8_t pack();
-    /// @brief Convert kicker command into a readible String format
-    /// @return String representation of kicker command
+
+    /**
+     * Convert kicker command into a readible String format
+     * 
+     * @return String representation of kicker command
+     */
     String to_string();
 };
 
@@ -53,10 +68,17 @@ struct KickerState {
     // False if kicker has errored or stopped responding
     bool healthy;
 
-    /// @brief Creates a KickerState from the byte returned by kicker SPI
-    /// @param state Byte response from the kicker
+    /**
+     * Creates a KickerState from the byte returned by kicker SPI
+     * 
+     * @param state Byte response from the kicker
+     */
     KickerState(uint8_t state);
-    /// @brief Converts kicker state into a readible String format
-    /// @return String representation of kicker state
+
+    /**
+     * Converts kicker state into a readible String format
+     * 
+     * @return String representation of kicker state
+     */
     String to_string();
 };
