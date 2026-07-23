68fea878d6990b30ff2a185ca4043d876a5fe952
Melon <dashjax9@gmail.com>
2026-07-23 00:30:08 -0500

    Added some missing docs

--- control/include/config.hpp
+++ control/include/config.hpp
@@ -11,9 +11,9 @@
 #define BATT_UVLO_THRESHOLD 10
 // Amount of time robot should keep moving without new command
 #define DIE_TIME_MS 1000
-
+// Frequency of the interval timer for motor control
 #define MOTION_FREQ_US 1000000 / 60 // 60hz
-
+// Frequency of the interval timer for kicker control
 #define KICKER_FREQ_US 1000000 / 30 // 30hz
-
+// Frequency of the interval timer for low priority tasks (ex: screen update, battery reading)
 #define LOW_PRIO_FREQ_US 1000000 / 2 // 2hz
