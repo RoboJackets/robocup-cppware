1003b83293a0d9371f0728074a6a91117a0bfe04
Dashjax <dashjax9@gmail.com>
2026-09-08 19:46:39 -0400

    Updated docs

--- control/src/main.cpp
+++ control/src/main.cpp
@@ -234,7 +234,6 @@ void motion_isr() {
   // Dribbler speed is prescaled (supposedly), zero if past die time
   int8_t dribbler_speed = (radio_timeout ? 0 : control_message.dribbler_speed);
   // Send commands to motor controllers
-  // TODO: Find real source of order reversal from rust version if necessary
   read_velocities(0) = motors[0].send_and_read(wheel_velocities(0));
   read_velocities(1) = motors[1].send_and_read(wheel_velocities(1));
   read_velocities(2) = motors[2].send_and_read(wheel_velocities(2));
